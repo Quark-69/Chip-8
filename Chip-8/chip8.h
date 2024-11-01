60ba8641f4d6fae5dac8c25c606c283afee0ef6a
UjjawalK7895 <ujjawal.kchouhan.ece23@itbhu.ac.in>
2024-11-01 13:42:40 +0530

    Add project files.

---
+++ Chip-8/chip8.h
@@ -0,0 +1,142 @@
+#pragma once
+#include <cstdint>
+#include <fstream>
+#include <random>
+#include <chrono>
+#include <filesystem>
+
+using uint = unsigned int;
+
+constexpr uint START_ADDRESS = 0x200;
+
+constexpr uint FONTSET_SIZE = 80;
+
+constexpr uint FONTSET_START_ADDRESS = 0x50;
+
+constexpr uint VIDEO_WIDTH = 64;
+
+constexpr uint VIDEO_HEIGHT = 32;
+
+const unsigned int KEY_COUNT = 16;
+const unsigned int MEMORY_SIZE = 4096;
+const unsigned int REGISTER_COUNT = 16;
+const unsigned int STACK_LEVELS = 16;
+
+
+
+constexpr uint8_t fontset[FONTSET_SIZE] = {
+	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
+class Chip8
+{
+public:
+	Chip8();
+	~Chip8();
+	void LoadROM(const char* fileName);
+
+private:
+	void OP_00E0();
+	void OP_00EE();
+	void OP_1nnn();
+	void OP_2nnn();
+	void OP_3xkk();
+	void OP_4xkk();
+	void OP_5xy0();
+	void OP_6xkk();
+	void OP_7xkk();
+	void OP_8xy0();
+	void OP_8xy1();
+	void OP_8xy2();
+	void OP_8xy3();
+	void OP_8xy4();
+	void OP_8xy5();
+	void OP_8xy6();
+	void OP_8xy7();
+	void OP_8xyE();
+	void OP_9xy0();
+	void OP_Annn();
+	void OP_Bnnn();
+	void OP_Cxkk();
+	void OP_Dxyn();
+	void OP_Ex9E();
+	void OP_ExA1();
+	void OP_Fx07();
+	void OP_Fx0A();
+	void OP_Fx15();
+	void OP_Fx18();
+	void OP_Fx1E();
+	void OP_Fx29();
+	void OP_Fx33();
+	void OP_Fx55();
+	void OP_Fx65();
+
+	void Table0()
+	{
+		((*this).*(table0[opcode & 0x000Fu]))();
+	}
+
+	void Table8()
+	{
+		((*this).*(table8[opcode & 0x000Fu]))();
+	}
+
+	void TableE()
+	{
+		((*this).*(tableE[opcode & 0x000Fu]))();
+	}
+
+	void TableF()
+	{
+		((*this).*(tableF[opcode & 0x00FFu]))();
+	}
+
+	void OP_NULL()
+	{}
+
+
+public:
+	uint32_t* getVideo();
+	uint8_t* getKeypad();
+
+	void Cycle();
+
+private:
+	uint8_t registers[REGISTER_COUNT]{};
+	uint8_t* memory;
+	uint16_t index_register{};
+	uint16_t pc{};
+	uint16_t stack[STACK_LEVELS]{};
+	uint8_t sp{};
+	uint8_t delay_timer{};
+	uint8_t sound_timer{};
+	uint8_t keypad[KEY_COUNT]{};
+	uint32_t* video;
+	uint16_t opcode{};
+
+	std::mt19937 randGen;
+	std::uniform_int_distribution<uint> randByte;
+
+	typedef void (Chip8::* Chip8Func)();
+	Chip8Func table[0xF + 1];
+	Chip8Func table0[0xE + 1];
+	Chip8Func table8[0xE + 1];
+	Chip8Func tableE[0xE + 1];
+	Chip8Func tableF[0x65 + 1];
+	
+};
